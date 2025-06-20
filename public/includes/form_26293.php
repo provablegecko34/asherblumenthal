8d841de399e356311725c23b13f658768b43156c
provablegecko34 <153940045+provablegecko34@users.noreply.github.com>
2025-06-20 19:28:16 -0400

    changed redirect to icloud  SMTB

--- public/includes/form_26293.php
+++ public/includes/form_26293.php
@@ -1,4 +1,12 @@
 <?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\SMTP;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'PHPMailer/Exception.php';
+	require 'PHPMailer/PHPMailer.php';
+	require 'PHPMailer/SMTP.php';
+
 	if (empty($_POST['name_24998_26293']) && strlen($_POST['name_24998_26293']) == 0 || empty($_POST['email_24998_26293']) && strlen($_POST['email_24998_26293']) == 0 || empty($_POST['message_24998_26293']) && strlen($_POST['message_24998_26293']) == 0)
 	{
 		return false;
@@ -7,25 +15,47 @@
 	$name_24998_26293 = $_POST['name_24998_26293'];
 	$email_24998_26293 = $_POST['email_24998_26293'];
 	$message_24998_26293 = $_POST['message_24998_26293'];
-	
+
+	/** SMTP Server Credentials **/
+	$smtp_host = 'smtp.mail.me.com';
+	$smtp_username = 'asherblumenthal@icloud.com';
+	$smtp_password = 'hjck-mwgw-noep-ujsf';
+	$smtp_port = '587';
+
 	// Create Message	
 	$to = 'asherblumenthal@icloud.com';
+	$replyTo = $email_24998_26293;
 	$email_subject = "New Contact Form Submission";
-	$email_body = "You have received a new message. \n\nName_24998_26293: $name_24998_26293 \nEmail_24998_26293: $email_24998_26293 \nMessage_24998_26293: $message_24998_26293 \n";
-	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
-	$headers .= "From: asherblumenthal@icloud.com\r\n";
-	$headers .= "Reply-To: $email_24998_26293";
+	$email_body = "You have received a new message. <br><br>Name_24998_26293: $name_24998_26293 <br>Email_24998_26293: $email_24998_26293 <br>Message_24998_26293: $message_24998_26293 <br>";
 
-	// Post Message
-	if (function_exists('mail'))
-	{
-		$result = mail($to,$email_subject,$email_body,$headers);
+	$mail = new PHPMailer(true);
+
+	try {
+		$mail->isSMTP();
+		$mail->CharSet = 'utf-8';
+		$mail->Host = $smtp_host;
+		$mail->SMTPAuth = true;
+		$mail->Username = $smtp_username;
+		$mail->Password = $smtp_password;
+		$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; /** or PHPMailer::ENCRYPTION_SMTPS for SSL **/
+		$mail->Port = $smtp_port;
+
+		$mail->setFrom($smtp_username, 'Blocs Website');
+		$mail->addAddress($to);
+    	$mail->addReplyTo($replyTo, 'Reply To');
+
+		$mail->isHTML(true);
+		$mail->Subject = $email_subject;
+		$mail->Body = $email_body;
+
+		$mail->send();
+		exit;
 	}
-	else // Mail() Disabled
+	catch (Exception $e)
 	{
-		$error = array("message" => "The php mail() function is not available on this server.");
-	    header('Content-Type: application/json');
-	    http_response_code(500);
-	    echo json_encode($error);
-	}	
+		$error = array("message" => 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo);
+		header('Content-Type: application/json');
+		http_response_code(500);
+		echo json_encode($error);
+	}
 ?>
