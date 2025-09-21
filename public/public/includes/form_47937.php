0a86887b3d7e4cb6d3211481459dc11f9427ed09
provablegecko34 <153940045+provablegecko34@users.noreply.github.com>
2025-09-20 20:39:54 -0500

    removed + simplified pages
    added "cat" page

---
+++ public/public/includes/form_47937.php
@@ -0,0 +1,31 @@
+<?php
+	if (empty($_POST['name_24998_47937']) && strlen($_POST['name_24998_47937']) == 0 || empty($_POST['email_24998_47937']) && strlen($_POST['email_24998_47937']) == 0 || empty($_POST['message_24998_47937']) && strlen($_POST['message_24998_47937']) == 0)
+	{
+		return false;
+	}
+	
+	$name_24998_47937 = $_POST['name_24998_47937'];
+	$email_24998_47937 = $_POST['email_24998_47937'];
+	$message_24998_47937 = $_POST['message_24998_47937'];
+	
+	// Create Message	
+	$to = 'blumenthala@beloit.edu';
+	$email_subject = "Message from asherblumenthal.com";
+	$email_body = "You have received a new message! \n\nName_24998_47937: $name_24998_47937 \nEmail_24998_47937: $email_24998_47937 \nMessage_24998_47937: $message_24998_47937 \n";
+	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
+	$headers .= "From: Contact@asherblumenthal.com\r\n";
+	$headers .= "Reply-To: $email_24998_47937";
+
+	// Post Message
+	if (function_exists('mail'))
+	{
+		$result = mail($to,$email_subject,$email_body,$headers);
+	}
+	else // Mail() Disabled
+	{
+		$error = array("message" => "The php mail() function is not available on this server.");
+	    header('Content-Type: application/json');
+	    http_response_code(500);
+	    echo json_encode($error);
+	}	
+?>
